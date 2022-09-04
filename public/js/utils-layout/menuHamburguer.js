15bdafaabd41967b2b1c60ab012c825893205e76
Mauricio-Maxuel <mauricio.msantos@sptech.school>
2022-09-04 18:31:25 -0300

    fix/ responsividade quase pronta, ajustar tamanho das fontes e imagens

---
+++ public/js/utils-layout/menuHamburguer.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const headerHamb = document.getElementById('headerHamb');
+    headerHamb.classList.toggle('active')    
+}
+
+function removeActiveHamburguer(){
+    const headerHamb = document.getElementById('headerHamb');
+    headerHamb.classList.remove('active') 
+}
